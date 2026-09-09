59b42a300bb59a5ec158d3234ddab03858e447ec
mirror344 <dudakovivan3@gmail.com>
2026-09-10 00:05:23 +0300

    learn dart syntax and types

--- bin/lab1_dart.dart
+++ bin/lab1_dart.dart
@@ -29,4 +29,22 @@ void main() {
   String display = nicname ?? 'Аноним';
   print(display);
 
+  List<String> fruits = ['яблоко', 'банан', 'груша'];
+  fruits.add('апельсин');
+  print(fruits[0]);
+  print(fruits.length);
+
+  Map<String, dynamic> person = {'name': 'Артём', 'age': 20};
+  print(person['name']);
+  person['city'] = 'Волжский';
+
+  Set<int> ids = {1, 2, 3, 2, 1};
+  print(ids);
+  print(ids.length);
+
+  List<String> fruits2 = ['яблоко', 'банан', 'груша'];
+  for (var fruit in fruits2) {
+    print(fruit);
+  }
+
 }
